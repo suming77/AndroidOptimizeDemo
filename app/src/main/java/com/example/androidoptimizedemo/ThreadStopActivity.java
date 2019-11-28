9bc9281f27075992b78cad0dd1cc28febf1daa5a
sumingyan <su_mingyan@163.com>
2019-11-28 11:36:16 +0800

    线程停止方式

--- app/src/main/java/com/example/androidoptimizedemo/ThreadStopActivity.java
+++ app/src/main/java/com/example/androidoptimizedemo/ThreadStopActivity.java
@@ -8,7 +8,7 @@ import android.util.Log;
 /**
  * @创建者 mingyan.su
  * @创建时间 2019/11/25 17:48
- * @类描述 ${TODO}线程停止
+ * @类描述 ${TODO}线程停止dsfsdf
  */
 public class ThreadStopActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName();
